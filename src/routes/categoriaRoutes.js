95bb6dabba7cf0e12b6606feff1ae1bacf32b01b
KAICK EDUARDO ARAUJO DA SILVA <kaick.silva4@senaisp.edu.br>
2026-06-22 08:07:53 -0300

    prontooo

--- src/routes/categoriaRoutes.js
+++ src/routes/categoriaRoutes.js
@@ -4,7 +4,7 @@ import categoriaController from "../controllers/categoriaController.js";
 const categoriaRoutes = Router();
 
 categoriaRoutes.post('/', categoriaController.criar);
-categoriaRoutes.put('/', categoriaController.atualizar);
+categoriaRoutes.put('/:id', categoriaController.atualizar);
 categoriaRoutes.get('/', categoriaController.selecionar)
 categoriaRoutes.delete('/:id', categoriaController.deletar);
 
